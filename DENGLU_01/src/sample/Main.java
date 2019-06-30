c3e2632fbe0fd2afbd6ab23c511af5ea353836ee
yzhdut <52197936+yzhdut@users.noreply.github.com>
2019-06-30 19:24:22 +0800

    javafx_by_yzh

---
+++ DENGLU_01/src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("mycss.css").toExternalForm());
+        primaryStage.setTitle("Hello World");
+
+        primaryStage.setScene(scene);
+        root.setId("background");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
